1fd78ce674e16c230aa122298fe256daf3b31a7a
Angelo Ortiz <angortizt@gmail.com>
2022-03-14 12:59:28 +0100

    Begin KMeans

--- layers.py
+++ layers.py
@@ -1,5 +1,5 @@
 """
-.. module:: kernels
+.. module:: layers
    :synopsis: This file contains ...
 .. moduleauthor:: Angelo Ortiz <github.com/angelo-ortiz>
 """
@@ -8,7 +8,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# from utils import k_means
+from utils import batch_norm, KMmeans
 # from solver import l_bfgs_b, sgd
 
 MIN_NORM = 1e-3
@@ -39,18 +39,22 @@ class CKN(nn.Module):
 
 class CKNLayer:
     def __init__(self, out_filters, patch_size, subsample_factor,
-                 solver_samples, device, var=None):
+                 solver_samples, var=None):
         self.out_filters = out_filters  # p_k
         self.patch_size = patch_size  # sqrt(|P'_k|)
         self.subsample_factor = subample_factor
         self.solver_samples = solver_samples
-        self.device = device
         self.var = var
 
-    def train(self, input_maps): # batch, h, w, c
-        if sth: # small dimension
-            pass
-        else: # high dimension
+    def train(self, input_maps):
+        """TODO
+
+        Parameters
+        ----------
+        input_maps : Tensor[batch, h, w, c]
+            The input maps `xi_{k-1}`.
+        """
+        if input_maps.shape[-1] > 2:  # high dimension
             patches = extract_sq_patches(input_maps, self.patch_size, 1)
             # patches = patches.view(
             #     -1, np.prod(patches.shape[1:])
@@ -61,39 +65,50 @@ class CKNLayer:
                                             (2, self.solver_samples))
 
             X, Y = patches[indices[sampled_indices]]
-            w0 = k_means(torch.cat(X, Y), dim=0)
+            w0 = k_means(torch.cat((X, Y), dim=0), self.out_filters)
             self.w, self.eta, self.var = sgd(w0, X, Y, self.var)
         # if self.sigma is None:
         #     self.sigma = q_01(...)
 
     def forward(self, input_maps):
         n = input_maps.shape[0]
-        if sth: # small dimension
+        if input_maps.shape[-1] <= 2:  # low dimension
             pass
-        else: # high dimension
+        else:  # high dimension
             patches = extract_sq_patches(input_maps, self.patch_size, 1)
-            norms = torch.linalg.norm(patches, dim=(1,2,3))
-            norms = torch.maximum(MIN_NORM, norms)
-            patches_norm = patches / norms.view(-1, 1, 1, 1)
+            norms = batch_norm(patches)
+            patches_norm = patches \
+                / torch.maximum(MIN_NORM, norms).view(-1, 1, 1, 1)
+
             diffs = patches_norm.unsqueeze(1) - self.w
-            diffs_norm = torch.einsum('pohwc,ohwc->po', diffs, diffs)
-            act_maps = np.exp(-diffs_norm/var) * torch.sqrt(self.eta)
-            act_maps *= norms.unsqueeze(1) # batch * h'* w', p_k
-            gauss_window = gaussian_window( # h", w"
-                self.subsample_factor,
-                self.subsample_factor**2,
-                self.device
+            diffs_norm = torch.einsum('pohwi,ohwi->po', diffs, diffs)
+
+            act_maps = torch.exp(-diffs_norm/var) * torch.sqrt(self.eta)
+            act_maps *= norms.unsqueeze(1) # batch * h * w, p_k
+
+            gauss_window = gaussian_window( # h', w'
+                n=self.subsample_factor,
+                var=self.subsample_factor**2,
+                device=input_maps.device
             )
-            num_patches_side = np.sqrt(act_maps.shape[0]/n)
-            assert num_patches_side == int(num_patches_side)
-            num_patches_side = int(num_patches_side)
-            tensor = act_maps.view(n, num_patches_side, num_patches_side, self.out_filters)
-            tensor = tensor.permute(0, 3, 1, 2).contiguous()
-            tensor = tensor.view(-1, 1, num_patches_side, num_patches_side) # batch * p_k, 1, h', w'
+
+            patch_dim = np.sqrt(act_maps.shape[0]/n)
+            assert patch_dim == int(patch_dim)
+            patch_dim = int(patch_dim)
+
+            act_maps = act_maps.view(n, patch_dim, patch_dim, self.out_filters)
+            act_maps = act_maps.permute(0, 3, 1, 2).contiguous()
+            act_maps = act_maps.view(-1, 1, patch_dim, patch_dim)
+
             output_maps = F.conv2d(
-                tensor,
-                gauss_window.view(1, 1, *gaussian_window.shape),
-                self.subsample_factor
+                act_maps,  # batch * p_k, 1, h, w
+                gauss_window.view(1, 1, *gaussian_window.shape),  # 1, 1, h', w'
+                stride=self.subsample_factor
             )
-            output_maps = out_maps.view(n, self.out_filters, *output_maps.shape[2:]).permute(0, 2, 3, 1).contiguous()
-            return output_maps * np.sqrt(2. / np.pi) # batch, p_k, h3, w3
+            output_maps = out_maps.view(
+                n,
+                self.out_filters,
+                *output_maps.shape[2:]
+            ).permute(0, 2, 3, 1).contiguous()
+
+            return output_maps * np.sqrt(2./np.pi) # batch, p_k, h", w"
---
+++ utils.py
@@ -0,0 +1,238 @@
+"""
+.. module:: utils
+   :synopsis: This file contains ...
+.. moduleauthor:: Angelo Ortiz <github.com/angelo-ortiz>
+"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def batch_norm(X, squared=False):
+    norms = torch.einsum('bhwc,bhwc->b', X, X)
+
+    if not squared:
+        torch.sqrt(norms, norms)
+    return norms
+
+
+def euclidean_distances(X, Y, X_norm_squared=None, Y_norm_squared=None,
+                        squared=False):
+    if X_norm_squared is None:
+        X_norm_squared = batch_norm(X, squared=True)
+    XX = X_norm_squared.unsqueeze(1)
+
+    if Y_norm_squared is None:
+        Y_norm_squared = batch_norm(Y, squared=True)
+    YY = Y_norm_squared.unsqueeze(0)
+
+    distances = -2. * torch.einsum('xhwc,yhwc->xy', X, Y)
+    distances += XX
+    distances += YY
+    torch.maximum(distances, 0, out=distances)
+
+    if not squared:
+        torch.sqrt(distances, distances)
+
+    return distances
+
+
+def _init_centroids(X, n_clusters, x_sq_norms):
+    n_samples, h, w, c = X.shape
+    centres = torch.empty(
+        n_clusters, h, w, c, dtype=X.dtype, device=X.device
+    )
+
+    n_local_trials = 2 + int(np.log(n_clusters))
+
+    # Pick first center randomly and track index of point
+    centre_id = np.random.randint(n_samples)
+    # indices = torch.full(
+    #     size=n_clusters, fill_value=-1, dtype=torch.int32, device=X.device
+    # )
+    centres[0] = X[centre_id]
+    # indices[0] = centre_id
+
+    # Initialise list of closest distances and calculate current potential
+    closest_dist_sq = euclidean_distances(
+        centres[0].unsqueeze(0), X, Y_norm_squared=x_sq_norms, squared=True
+    )
+    current_pot = closest_dist_sq.sum()
+
+    # Pick the remaining n_clusters-1 points
+    for c in range(1, n_clusters):
+        # Choose centre candidates by sampling with probability proportional
+        # to the squared distance to the closest existing centre
+        rand_vals = torch.rand(n_local_trials, device=X.device) * current_pot
+        candidate_ids = torch.searchsorted(torch.cumsum(closest_dist_sq), rand_vals)
+        # XXX: numerical imprecision can result in a candidate_id out of range
+        torch.clip(candidate_ids, None, closest_dist_sq.numel - 1, out=candidate_ids)
+
+        # Compute distances to center candidates
+        distance_to_candidates = euclidean_distances(
+            X[candidate_ids], X, Y_norm_squared=x_sq_norms, squared=True
+        )
+
+        # update closest distances squared and potential for each candidate
+        torch.minimum(closest_dist_sq, distance_to_candidates, out=distance_to_candidates)
+        candidates_pot = distance_to_candidates.sum(dim=1)
+
+        # Decide which candidate is the best
+        best_candidate = torch.argmin(candidates_pot).item()
+        current_pot = candidates_pot[best_candidate]
+        closest_dist_sq = distance_to_candidates[best_candidate]
+        best_candidate = candidate_ids[best_candidate]
+
+        # Permanently add best center candidate found in local tries
+        centres[c] = X[best_candidate]
+        # indices[c] = best_candidate
+
+    return centers# , indices
+
+
+def _is_same_clustering(labels1, labels2, n_clusters):
+    mapping = torch.full(
+        size=n_clusters, fill_value=-1, dtype=torch.int32, device=labels1.device
+    )
+
+    for i in range(len(labels1)):
+        if mapping[labels1[i]] == -1:
+            mapping[labels1[i]] = labels2[i]
+        elif mapping[labels1[i]] != labels2[i]:
+            return False
+    return True
+
+def _inertia(X, centres, labels):
+    # TODO
+    return inertia
+
+def _kmeans_iter(X, x_sq_norms, centres, update_centres=True):
+    n_clusters = len(centres)
+    centres_new = torch.zeros_like(centres)
+    labels = torch.full(
+        size=len(X), fill_value=-1, dtype=torch.int32, device=X.device
+    )
+    weight_in_clusters = torch.zeros(
+        n_clusters, dtype=X.dtype, device=X.device
+    )
+    centre_shift = torch.zeros_like(weight_in_clusters)
+    # TODO
+
+    return centres_new, weight_in_clusters, labels, centre_shift
+
+
+def _kmeans_single(X, x_sq_norms, centres, max_iter=300, tol=1e-4):
+    labels_old = None
+    # labels.clone()
+    strict_convergence = False
+
+    for i in range(max_iter):
+        centres_new, weight_in_clusters, labels, centre_shift = _kmeans_iter(
+            X,
+            x_sq_norms,
+            centres
+        )
+
+        centres, centres_new = centres_new, centres
+
+        if labels_old is not None and torch.equal(labels, labels_old):
+            # First check the labels for strict convergence.
+            strict_convergence = True
+            break
+        else:
+            # No strict convergence, check for tol-based convergence.
+            centre_shift_tot = torch.square(centre_shift).sum()
+            if center_shift_tot <= tol:
+                break
+
+        if labels_old is None:
+            labels_old = labels.clone()
+        else:
+            labels_old[:] = labels
+
+    if not strict_convergence:
+        # rerun E-step so that predicted labels match cluster centers
+        _, _, labels, _ = _kmeans_iter(
+            X,
+            x_sq_norms,
+            centres,
+            update_centres=False
+        )
+
+    inertia = _inertia(X, centres, labels)
+
+    return labels, inertia, centres, i + 1
+
+
+class KMeans:
+    """
+    Parameters
+    ----------
+    n_clusters : int, > 0
+        The number of clusters to form as well as the number of centroids
+        to generate.
+    n_init : init, default=10
+        Number of time the k-means algorithm will be run with different
+        centroid seeds. The final results will be the best output of
+        n_init consecutive runs in terms of inertia.
+    max_iter : int, default=300
+        Maximum number of iterations of the k-means algorithm for a
+        single run.
+    tol : float, default=1e-4
+        Relative tolerance with regards to Frobenius norm of the difference
+        in the cluster centers of two consecutive iterations to declare
+        convergence.
+    """
+    def __init__(self, n_clusters, n_init=10, max_iter=300, tol=1e-4):
+        self.n_clusters = n_clusters
+        self.n_init = n_init
+        self.max_iter = max_iter
+        self.tol = tol
+
+    def fit(self, X):
+        """
+        Parameters
+        ----------
+        X : Tensor[batch, h, w, c]
+            The data points.
+        """
+        x_squared_norms = batch_norms(X, squared=True)
+        best_inertia, best_labels = None, None
+
+        # subtract of mean of x for more accurate distance computations
+        X_mean = X.mean(dim=0)
+        X = X - X_mean  # avoid overwriting the given tensor
+
+        for i in range(self.n_init):
+            centres = _init_centroids(X, self.n_clusters, x_sq_norms)
+            labels, inertia, centres, n_iter_ = _kmeans_single(
+                X,
+                x_sq_norms,
+                centres,
+                max_iter=self.max_iter,
+                tol=self.tol,
+            )
+
+            if best_inertia is None or (
+                    inertia < best_inertia
+                    and not _is_same_clustering(labels, best_labels, self.n_clusters)
+            ):
+                best_labels = labels
+                best_centres = centres
+                best_inertia = inertia
+                best_n_iter = n_iter_
+
+        best_centres += X_mean
+
+        distinct_clusters = len(set(best_labels))
+        if distinct_clusters < self.n_clusters:
+            print(f'Warning: Number of distinct clusters ({distinct_clusters})',
+                  f'found less than n_clusters ({self.n_clusters})!', sep=' ')
+
+        self.cluster_centres_ = best_centres
+        self.labels_ = best_labels
+        self.inertia_ = best_inertia
+        self.n_iter_ = best_n_iter
+
+        return self
